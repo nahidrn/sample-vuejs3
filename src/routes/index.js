790f5cc5a79d73c6dc0b07537144ac3e317be06c
Nahid Rahman <sm.nahid.rn@gmail.com>
2021-04-04 20:58:34 +0600

    Added route guard to ThreadShow route for redirecting user if the thread id doesn't exist

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import PageThreadShow from '@/components/PageThreadShow'
 import PageNotFound from '@/components/PageNotFound'
 //import * as VueRouter from 'vue-router'
 import { createRouter, createWebHistory } from 'vue-router'
+import sourceData from "@/data.json"
 
 const routes = [
     {
@@ -14,7 +15,24 @@ const routes = [
         path: '/thread/:id',
         name: 'ThreadShow',
         component: PageThreadShow,
-        props: true
+        props: true,
+        beforeEnter (to, from, next) {
+            // check if thread exists
+            const threadExists = sourceData.threads.find(thread => thread.id === to.params.id)
+            //if exists continue
+            // if doesn't exist redirect to not found
+            if(threadExists) {
+                return next()
+            } else {
+                // to not change the url so the user can review the wrongly entered url
+                next({
+                    name: 'NotFound',
+                    params: { pathMatch: to.path.substring(1).split('/') },
+                    query: to.query,
+                    hash: to.hash
+                })
+            }
+        }
     },
     { 
         path: '/:pathMatch(.*)*', 
