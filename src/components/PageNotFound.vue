cd65ad8a3ea0c821bbe6a416051f107c6843e273
Nahid Rahman <sm.nahid.rn@gmail.com>
2021-04-04 20:38:21 +0600

    Configured soft 404 route to show Not Found page

---
+++ src/components/PageNotFound.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="col-full text-center">
+        <h1>Not Found</h1>
+        <router-link :to="{name: 'Home'}">Read something cool from here</router-link>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
